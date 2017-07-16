33a2c448eaf9c3e4b66693a933311ffb3e7f072d
RocherKong <rocher@126.com>
2017-07-16 22:41:49 +0800

    add error for test

---
+++ Bara/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bara
+{
+    public class Error
+    {
+        public void ThrowError() {
+            throw new Exception("just for test");
+        }
+    }
+}
