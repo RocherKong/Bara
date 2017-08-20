85abc4f4daf785611aa8a14d3a953b853bd328c3
RocherKong <rocher@126.com>
2017-08-20 11:18:31 +0800

    add tag:IsGreaterEqual.cs IsLessEqual.cs IsLessThan.cs

---
+++ Bara/Core/Tags/IsGreaterEqual.cs
@@ -0,0 +1,25 @@
+﻿using Bara.Abstract.Tag;
+using Bara.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bara.Core.Tags
+{
+    public class IsGreaterEqual : CompareTag
+    {
+        public override TagType Type => TagType.IsGreaterEqual;
+
+        public override bool IsNeedShow(object objParam)
+        {
+            var reqVal = objParam.GetValue(Property);
+            bool isNeedShow = false;
+            if (reqVal == null) return false;
+            if (!Decimal.TryParse(reqVal.ToString(), out decimal reqValue)) { return false; };
+            if (!Decimal.TryParse(CompareValue.ToString(), out decimal compareValue)) { return false; };
+            if (reqValue >= compareValue) { return true; };
+            return isNeedShow;
+
+        }
+    }
+}
---
+++ Bara/Core/Tags/IsLessThan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bara.Abstract.Tag;
+using Bara.Common;
+
+namespace Bara.Core.Tags
+{
+    public class IsLessThan : CompareTag
+    {
+        public override TagType Type => TagType.IsLessThan;
+
+        public override bool IsNeedShow(object objParam)
+        {
+            var reqVal = objParam.GetValue(Property);
+            bool isNeedShow = false;
+            if (!Decimal.TryParse(reqVal.ToString(), out decimal reqValue)) { return false; };
+            if (!Decimal.TryParse(CompareValue, out decimal compareValue)) { return false; };
+            if (reqValue < compareValue) { return true; };
+            return isNeedShow;
+        }
+    }
+}
