5897eb34f866ba8800f0736c04c04d92521c8877
Rocher <rocher@126.com>
2017-08-23 15:43:55 +0800

    add SqlBuilder

---
+++ Bara/Abstract/SqlBuilder/ISqlBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Bara.Core.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bara.Abstract.SqlBuilder
+{
+    public interface ISqlBuilder
+    {
+        String BuildSql(RequestContext context);
+    }
+}
---
+++ Bara/Core/SqlBuilder/SqlBuilder.cs
@@ -0,0 +1,42 @@
+﻿using Bara.Abstract.Core;
+using Bara.Abstract.SqlBuilder;
+using Bara.Core.Context;
+using Bara.Exceptions;
+using Bara.Model;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bara.Core.SqlBuilder
+{
+    public class SqlBuilder : ISqlBuilder
+    {
+        private ILogger _logger;
+        private IBaraMapper _baraMapper { get; set; }
+
+        private IDictionary<String, Statement> MappedStatements => _baraMapper.BaraMapConfig.MappedStatements;
+        public SqlBuilder(ILoggerFactory loggerFactory, IBaraMapper baraMapper)
+        {
+            this._logger = loggerFactory.CreateLogger<SqlBuilder>();
+            this._baraMapper = baraMapper;
+        }
+
+        public String BuildSql(RequestContext context)
+        {
+            if (!MappedStatements.ContainsKey(context.FullSqlId)) {
+                throw new BaraException($"Can't Find Sql:{context.FullSqlId} in MappedStatements.Please Check Name or .Xml file.");
+            }
+            var stateMent = MappedStatements[context.FullSqlId];
+            return GenerateSql(context, stateMent);
+        }
+
+        public string GenerateSql(RequestContext context, Statement statement) {
+            if (statement == null) {
+                throw new BaraException($"Can't Find Sql:{context.FullSqlId} in MappedStatements.Please Check Name or .Xml file.");
+            }
+            return statement.BuildSql(context);
+        }
+
+    }
+}
