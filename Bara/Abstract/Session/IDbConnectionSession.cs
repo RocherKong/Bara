ea477478700ad318f5babd9c5511bc6806b9deca
Rocher <rocher@126.com>
2017-08-23 09:27:58 +0800

    Test change to core 1.1

--- Bara/Abstract/Session/IDbConnectionSession.cs
+++ Bara/Abstract/Session/IDbConnectionSession.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Bara.Abstract.DataSource;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Text;
 
 namespace Bara.Abstract.Session
