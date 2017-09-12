2c5fff182a13d3e5f95d2adb651b7ab48fff350a
Rocher <rocher@126.com>
2017-09-12 22:13:13 +0800

    Add Cache/CacheManager.cs

---
+++ Bara/Abstract/Cache/ICacheManager.cs
@@ -0,0 +1,28 @@
+﻿using Bara.Abstract.Core;
+using Bara.Core.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bara.Abstract.Cache
+{
+    public interface ICacheManager
+    {
+        IBaraMapper BaraMapper { get; set; }
+
+        object this[RequestContext context, Type type] { get; set; }
+
+        void ResetMappedCaches();
+
+        void TriggerFlush(RequestContext context);
+
+        Queue<RequestContext> RequestQueue { get; set; }
+
+        void FlushQueue();
+
+        void ClearQueue();
+
+
+
+    }
+}
---
+++ Bara/Core/Cache/CacheManager.cs
@@ -0,0 +1,45 @@
+﻿using Bara.Abstract.Cache;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Bara.Abstract.Core;
+using Bara.Core.Context;
+using Microsoft.Extensions.Logging;
+
+namespace Bara.Core.Cache
+{
+    public class CacheManager : ICacheManager
+    {
+        private readonly ILogger _logger;
+        public CacheManager(ILoggerFactory loggerFactory, IBaraMapper baraMapper)
+        {
+            this._logger = loggerFactory.CreateLogger<CacheManager>();
+            this.BaraMapper = baraMapper;
+            RequestQueue = new Queue<RequestContext>();
+        }
+        public object this[RequestContext context, Type type] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IBaraMapper BaraMapper { get; set; }
+        public Queue<RequestContext> RequestQueue { get; set; }
+
+        public void ClearQueue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FlushQueue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetMappedCaches()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TriggerFlush(RequestContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
