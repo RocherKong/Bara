4523522c708266e10d28e272ffa626ac34003776
Rocher <rocher@126.com>
2017-09-13 15:52:46 +0800

    CacheManager load MappedTriggerFlushs

--- Bara/Core/Cache/CacheManager.cs
+++ Bara/Core/Cache/CacheManager.cs
@@ -5,18 +5,68 @@ using System.Text;
 using Bara.Abstract.Core;
 using Bara.Core.Context;
 using Microsoft.Extensions.Logging;
+using Bara.Model;
 
 namespace Bara.Core.Cache
 {
     public class CacheManager : ICacheManager
     {
         private readonly ILogger _logger;
+        private static readonly object syncobj = new object();
         public CacheManager(ILoggerFactory loggerFactory, IBaraMapper baraMapper)
         {
             this._logger = loggerFactory.CreateLogger<CacheManager>();
             this.BaraMapper = baraMapper;
             RequestQueue = new Queue<RequestContext>();
         }
+
+        private IDictionary<String, IList<Statement>> _mappedTriggerFlushs;
+        public IDictionary<String, IList<Statement>> MappedTriggerFlushs
+        {
+            get
+            {
+                if (_mappedTriggerFlushs == null)
+                {
+                    lock (syncobj)
+                    {
+                        if (_mappedTriggerFlushs == null)
+                        {
+                            _logger.LogDebug("CacheManger Start Load MappedTriggerFlushs.");
+                            _mappedTriggerFlushs = new Dictionary<String, IList<Statement>>();
+                            foreach (var baraMap in BaraMapper.BaraMapConfig.BaraMaps)
+                            {
+                                foreach (var statement in baraMap.Statements)
+                                {
+                                    if (statement.Cache == null
+                                        || statement.Cache.FlushOnExecutes == null)
+                                    {
+                                        continue;
+                                    }
+                                    foreach (var triggerStatement in statement.Cache.FlushOnExecutes)
+                                    {
+                                        IList<Statement> triggerStatements = null;
+                                        if (_mappedTriggerFlushs.ContainsKey(triggerStatement.Statement))
+                                        {
+                                            triggerStatements = _mappedTriggerFlushs[triggerStatement.Statement];
+                                        }
+                                        else
+                                        {
+                                            triggerStatements = new List<Statement>();
+                                            _mappedTriggerFlushs[triggerStatement.Statement] = triggerStatements;
+                                        }
+                                        triggerStatements.Add(statement);
+                                    }
+                                }
+                            }
+                            _logger.LogDebug("CacheManger End Load MappedTriggerFlushs.");
+                        }
+                    }
+                }
+
+                return _mappedTriggerFlushs;
+            }
+        }
+
         public object this[RequestContext context, Type type] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public IBaraMapper BaraMapper { get; set; }
