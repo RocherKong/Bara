426272ea3c91debe4991fe3c9c48799e253b8264
RocherKong <rocher@126.com>
2017-09-22 23:38:18 +0800

    Add DataAccess Interface

---
+++ Dev/Bara.DataAccess/Abstractions/IQuery.cs
@@ -0,0 +1,42 @@
+﻿using Bara.Abstract.DataSource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bara.DataAccess.Abstractions
+{
+    public interface IQuery<TEntity> : IExist, IGetEntity<TEntity>, IGetList, IGetListByPage, IGetRecord
+         where TEntity : class
+    {
+    }
+
+    public interface IExist {
+        bool IsExist(object reqParams, DataSourceType sourceType = DataSourceType.Read);
+    }
+
+    public interface IGetEntity<TEntity> where TEntity : class {
+        TEntity GetEntity<TPrimary>(TPrimary Id,DataSourceType sourceType=DataSourceType.Read);
+    }
+
+    /// <summary>
+    /// 获取列表
+    /// </summary>
+    public interface IGetList
+    {
+        IEnumerable<TResponse> GetList<TResponse>(object paramObj, DataSourceType sourceType = DataSourceType.Read);
+    }
+    /// <summary>
+    /// 分页
+    /// </summary>
+    public interface IGetListByPage
+    {
+        IEnumerable<TResponse> GetListByPage<TResponse>(object paramObj, DataSourceType sourceType = DataSourceType.Read);
+    }
+    /// <summary>
+    /// 获取记录数
+    /// </summary>
+    public interface IGetRecord
+    {
+        int GetRecord(object paramObj, DataSourceType sourceType = DataSourceType.Read);
+    }
+}
