66835428b3c7476159ab7f4d5ae5ffe59261b76f
RocherKong <rocher@126.com>
2017-11-05 21:43:51 +0800

    Support Dynamic Params

--- Dev/Bara/Common/ObjectExtension.cs
+++ Dev/Bara/Common/ObjectExtension.cs
@@ -7,9 +7,17 @@ namespace Bara.Common
 {
     public static class ObjectExtension
     {
-        public static Object GetValue(this Object obj, String PropertyName) {
-            //GetRuntimeProperty
-            return obj?.GetType().GetRuntimeProperty(PropertyName)?.GetValue(obj);
+        public static Object GetValue(this Object obj, String PropertyName)
+        {
+            var dynamicParams = obj as Dapper.DynamicParameters;
+            if (dynamicParams != null)
+            {
+                return dynamicParams.Get<object>(PropertyName);
+            }
+            else
+            {
+                return obj?.GetType().GetRuntimeProperty(PropertyName)?.GetValue(obj);
+            }
         }
     }
 }
